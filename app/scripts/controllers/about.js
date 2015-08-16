43d1ccc33bfdc9da2814241c5d1510efda017598
rafael.faundez <rafael.faundez@fpf.br>
2015-08-16 00:12:37 -0400

    estrutura inicial criada.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name classificadorUniasselviApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the classificadorUniasselviApp
+ */
+angular.module('classificadorUniasselviApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name classificadorUniasselviApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the classificadorUniasselviApp
+ */
+angular.module('classificadorUniasselviApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
